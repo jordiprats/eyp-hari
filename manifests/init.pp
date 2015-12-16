0439a06f1e986b461bd2d8c109d857b63661e045
Jordi Prats <jprats@systemadmin.es>
2015-12-16 14:27:28 +0100

    inicial

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+class hari ($file="/.hari", $content="HARi was HERE\n", $mode="0400") {
+        file { $file:
+                ensure  => present,
+                owner   => 'root',
+                group   => "root",
+                mode    => $mode,
+                content => $content,
+        }
+}
---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+describe 'hari' do
+
+  context 'with defaults for all parameters' do
+    it { should contain_class('hari') }
+  end
+end
