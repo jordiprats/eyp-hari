20f78c0e6c27ea3e7f8bca371dfc8323c820fa01
Jordi Prats <jprats@systemadmin.es>
2016-01-21 16:53:09 +0100

    bugfix rake spec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 describe 'hari' do
 
   context 'with defaults for all parameters' do
+    let :facts do
+    {
+            :osfamily => 'Debian',
+            :operatingsystem => 'Ubuntu',
+            :operatingsystemrelease => '14.0',
+    }
+    end
+
     it { should contain_class('hari') }
   end
 end
